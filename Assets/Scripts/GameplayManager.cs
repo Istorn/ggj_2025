dde4e04ccd7662905ca64b7613f616fe2846945a
WhySere <alessandro.serenita@gmail.com>
2025-01-25 22:13:29 +0100

    Added Sprites to player prefab, added player selection canvas to main menu

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class GameplayManager : MonoBehaviour
 {
     public CanvasManager canvasManager;
+    spawnObject objectSpawner;
     private void Start()
     {
         canvasManager.met_BlackoutOff();
+        objectSpawner = GetComponent<spawnObject>();
     }
     public void met_GameOver()
     {
         canvasManager.met_GameOver();
+        objectSpawner.isSpawningBonus = false;
+        // Interrompere lo spawn dei bonus
+        // Immobilizzare i player e disattivarne le collisioni
     }
 }
