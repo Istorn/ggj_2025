fb00320f286c1c39a8c6ebb98ea373856c40b67e
WhySere <axelflamer@hotmail.com>
2025-01-25 10:35:12 +0100

    Aggiunto TextMeshPro, creata scena test punteggio

---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class PointsManager : MonoBehaviour
+{
+    public int punteggioSquadra;
+    public TextMeshProUGUI punti_ui;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        met_ResetPunti();
+    }
+    
+    public void met_ResetPunti()
+    {
+        punteggioSquadra = 0;
+        punti_ui.text = "0";
+    }
+
+    public void met_AggiungiPunti(int puntiDaAggiungere)
+    {
+        punteggioSquadra += puntiDaAggiungere;
+        punti_ui.text = punteggioSquadra.ToString();
+    }
+}
